84f4b03e2bfd1104fb85bd58726c805c58346dbe
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2025-10-18 21:13:43 +0100

    task timer is done

---
+++ src/js/1-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from 'flatpickr';
+
+let userSelectedDate = null;
+let userSelectedSeconds = null;
+let timerId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0] || null;
+    const now = options.defaultDate;
+    userSelectedSeconds = Math.floor(userSelectedDate.getTime() / 1000);
+    userSelectedSeconds -= Math.floor(now.getTime() / 1000);
+    if (!userSelectedDate || userSelectedDate.getTime() <= now) {
+      window.alert('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+  onChange(selectedDates) {
+    userSelectedDate = selectedDates[0] || null;
+    const now = options.defaultDate;
+    userSelectedSeconds = Math.floor(userSelectedDate.getTime() / 1000);
+    userSelectedSeconds -= Math.floor(now.getTime() / 1000);
+    if (selectedDates[0] < now) {
+      return (startBtn.disabled = true);
+    } else if (selectedDates[0] > now) {
+      return (startBtn.disabled = false);
+    }
+  },
+};
+
+const startBtn = document.querySelector('[data-start]');
+const dataDaysEl = document.querySelector('[data-days]');
+const dataHoursEl = document.querySelector('[data-hours]');
+const dataMinutsEl = document.querySelector('[data-minutes]');
+const dataSecondEl = document.querySelector('[data-seconds]');
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', () => {
+  console.log('Start!');
+  updateTimer();
+  timerId = setInterval(updateTimer, 1000);
+});
+
+function updateTimer() {
+  const now = new Date();
+  const diff = userSelectedDate - now;
+
+  if (diff <= 0) {
+    clearInterval(timerId);
+    console.log('Time is up!');
+    return;
+  }
+  const { days, hours, minutes, seconds } = convertMs(diff);
+  console.log({ days, hours, minutes, seconds });
+
+  dataDaysEl.textContent = days;
+  dataHoursEl.textContent = hours;
+  dataMinutsEl.textContent = minutes;
+  dataSecondEl.textContent = seconds;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// clearInterval(intervalId);
